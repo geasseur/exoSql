3898f232815dbfe223a014c0dd7d937f702330a6
geasseur <baptiste.ventura@neuf.fr>
2017-09-19 09:45:57 +0200

    links added

--- php/miniChat.php
+++ php/miniChat.php
@@ -19,7 +19,7 @@
     <div id="listMessages">
       <?php
       try{
-          $bdd = new PDO('mysql:host=localhost;dbname=exoSql;charset=utf8', 'root', 'root');
+          $bdd = new PDO('mysql:host=localhost;dbname=ExoSql;charset=utf8', 'root', 'root');
           array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
         }
       catch (Exception $e){
